005e5a0ff79e3d8a0e1d9d7e531706c677c51eba
Tomas Kacalek <kacalek.tom@gmail.com>
2020-05-01 07:44:26 +0200

    Updates

---
+++ precache-manifest.08b06db3dec7cc190ab6a7e8fe619564.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a795c8cc2853d00e8ff2eb29e0b1276a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0221e068df43b59efeda",
+    "url": "/static/css/main.87f9f364.chunk.css"
+  },
+  {
+    "revision": "289b7b557df6e13f374d",
+    "url": "/static/js/2.a9395397.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/static/js/2.a9395397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0221e068df43b59efeda",
+    "url": "/static/js/main.bc489ff1.chunk.js"
+  },
+  {
+    "revision": "542ccb7981adb69e032d",
+    "url": "/static/js/runtime-main.a0d61584.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4aa3e4453d9814865da791924a207927",
+    "url": "/static/media/search.4aa3e445.svg"
+  }
+]);
