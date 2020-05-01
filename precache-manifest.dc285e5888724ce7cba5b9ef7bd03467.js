f1bdeacd62ccdf62893f7e8393ab07fff4d2bc78
Tomas Kacalek <kacalek.tom@gmail.com>
2020-05-01 07:54:41 +0200

    Updates

---
+++ precache-manifest.dc285e5888724ce7cba5b9ef7bd03467.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1edd0f8937ef9f5a2b98c0a49f23bb9",
+    "url": "/tunes/index.html"
+  },
+  {
+    "revision": "d1ff776e54da1b1612c1",
+    "url": "/tunes/static/css/main.5400e686.chunk.css"
+  },
+  {
+    "revision": "289b7b557df6e13f374d",
+    "url": "/tunes/static/js/2.a9395397.chunk.js"
+  },
+  {
+    "revision": "ef7f0128066dad638e14e7838f835a83",
+    "url": "/tunes/static/js/2.a9395397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1ff776e54da1b1612c1",
+    "url": "/tunes/static/js/main.bc489ff1.chunk.js"
+  },
+  {
+    "revision": "a71f6d61c1bd9d3017b4",
+    "url": "/tunes/static/js/runtime-main.f667c14d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tunes/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4aa3e4453d9814865da791924a207927",
+    "url": "/tunes/static/media/search.4aa3e445.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08b06db3dec7cc190ab6a7e8fe619564.js"
+  "/tunes/precache-manifest.dc285e5888724ce7cba5b9ef7bd03467.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tunes/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
